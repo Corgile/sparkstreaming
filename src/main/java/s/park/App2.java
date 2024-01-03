25c50c52be5a1a688d6a372aecc9b63b91ed8f2d
corgile <mashiro-e@qq.com>
2024-01-03 21:55:53 +0800

    update

--- src/main/java/s/park/App2.java
+++ src/main/java/s/park/App2.java
@@ -1,6 +1,9 @@
 package s.park;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -18,17 +21,25 @@ import static org.apache.spark.sql.functions.*;
 
 @Slf4j
 public class App2 {
-  //  private static final Logger myErrorLogger = Logger.getLogger(App2.class);
-//  private static final Logger myErrorLogger = Logger.getLogger("myErrorLogger");
+  private static final Logger myErrorLogger = Logger.getLogger(App2.class);
   static MySQLBatchWriter sqlBatchWriter = new MySQLBatchWriter();
 
   public static void main(String[] args) throws StreamingQueryException, TimeoutException {
 
+    SparkConf sparkConf = new SparkConf()
+        .setAppName("FlowWarningCalculationJob")
+        .set("spark.executor.heartbeatInterval", "18000ms")
+        .set("spark.sql.streaming.forceDeleteTempCheckpointLocation", "true")
+        .set("spark.sql.adaptive.enabled", "false")
+        .set("failOnDataLoss", "false")
+        .set("checkpointLocation", "hdfs://hadoop-master-146:8020/checkpoint")
+        .setMaster("local")
+        .setJars(new String[]{"hdfs://hadoop-master-146:8020/structured-streaming/jobs/latency-test.jar"});
+//        .setMaster("yarn");
 
     SparkSession spark = SparkSession
         .builder()
-        .appName("Kafka Spark Streaming Example")
-        .master("local")
+        .config(sparkConf)
         .getOrCreate();
 
     // 订阅 Kafka 主题
@@ -40,14 +51,13 @@ public class App2 {
                 "172.22.105.146:9092,172.22.105.147:9092," +
                 "172.22.105.150:9092,172.22.105.38:9092," +
                 "172.22.105.39:9092")
-        .option("kafka.group.id", "spark_consumers")
-        .option("subscribe", "flow-warning-msg")
+        .option("kafka.group.id", "spark_consumers_for_latency_test")
+        .option("subscribe", "debug-warn")
+        .option("failOnDataLoss", "false")
         .option("fetchOffset.numRetries", "3")
         .option("fetchOffset.retryIntervalMs", "1000")
         .load();
 
-    Dataset<Row> kafkaMessageKey = kafkaRawData
-        .selectExpr("CAST(key AS STRING)");
 
     StructType schema = Encoders.bean(AttackMessage.class).schema();
     Dataset<Row> mysqlDataFrame = kafkaRawData
@@ -59,37 +69,43 @@ public class App2 {
         .count()
         .toDF("srcIp", "dstIp", "attackType", "count");
 
-    log.warn("\033[34;1m Config MySQL writer ...\033[0m");
     StreamingQuery writeToMySQL = mysqlDataFrame
         .writeStream()
         .foreach(sqlBatchWriter)
         .outputMode(OutputMode.Update())
         .start();
 
+    /// key
+    Dataset<Row> kafkaMessageKey = kafkaRawData
+        .selectExpr("CAST(key AS STRING)");
     Dataset<Row> modifiedKey = kafkaMessageKey
         .withColumn("modifiedKey",
             concat(
-                regexp_replace(col("key"), "#", ","),
+                col("key"),
+                lit(","),
+                substring(col("key"), 42, 13),
                 lit(","),
                 lit(String.valueOf(System.currentTimeMillis()))
-            ))
-        .select("modifiedKey");
+            ));
 
     String hdfsPath = "hdfs://hadoop-master-146:8020/user/wyyiot/timestamp.csv";
     StreamingQuery writeToHDFS = modifiedKey
+        .select("modifiedKey")
         .writeStream()
         .outputMode(OutputMode.Append())
         .format("csv")
+//        .format("console")
         .option("path", hdfsPath)
-        // .foreachBatch(App2::call)
+//        .foreachBatch(App2::call)
         .option("checkpointLocation", "hdfs://hadoop-master-146:8020/checkpoint")
         .start();
 
+    log.warn("\033[34;1m ALL SET. \033[0m");
     writeToHDFS.awaitTermination();
     writeToMySQL.awaitTermination();
   }
 
-//  private static void call(Dataset<Row> batchDF, Long batchId) {
-//    batchDF.foreach((ForeachFunction<Row>) row -> myErrorLogger.error(row.getString(0)));
-//  }
+  private static void call(Dataset<Row> batchDF, Long batchId) {
+    batchDF.foreach((ForeachFunction<Row>) row -> myErrorLogger.error(row.getString(0)));
+  }
 }
